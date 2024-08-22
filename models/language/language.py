2be0ecc968f40bc8b3518b3176bd9029e174a4d0
TnTerry <terry.jiangtianxu@gmail.com>
2024-08-22 22:05:03 +0000

    Model structure

--- models/language/language.py
+++ models/language/language.py
@@ -1,18 +1,3 @@
-# # Load model directly
-# from transformers import AutoTokenizer, AutoModelForCausalLM
-
-# tokenizer = AutoTokenizer.from_pretrained("ProbeMedicalYonseiMAILab/medllama3-v20")
-# model = AutoModelForCausalLM.from_pretrained("ProbeMedicalYonseiMAILab/medllama3-v20")
-
-# # Use a pipeline as a high-level helper
-# from transformers import pipeline
-
-# messages = [
-#     {"role": "user", "content": "Who are you?"},
-# ]
-# pipe = pipeline("text-generation", model="ProbeMedicalYonseiMAILab/medllama3-v20")
-# pipe(messages)
-
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -21,12 +6,10 @@ class LMInject(nn.Module):
     def __init__(
         self,
         model_name_or_path:str,
-        knowledge_max_length:int
     ) -> None:
         super().__init__()
         self.model_name_or_path = model_name_or_path
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.knowledge_max_length = knowledge_max_length
 
         self.tokenizer = AutoTokenizer.from_pretrained(
             self.model_name_or_path
@@ -37,7 +20,8 @@ class LMInject(nn.Module):
     
     def generate_knowledge(
         self,
-        original_prompt:str
+        original_prompt:str,
+        max_length:int
     ) -> str:
         inputs = self.tokenizer(
             original_prompt, return_tensors="pt"
@@ -45,7 +29,7 @@ class LMInject(nn.Module):
 
         generate_ids = self.model.generate(
             inputs.input_ids, 
-            max_length=self.knowledge_max_length
+            max_length=max_length
         )
 
         message = self.tokenizer.batch_decode(
@@ -58,11 +42,21 @@ class LMInject(nn.Module):
 if __name__ == "__main__":
     model_lm_inject = LMInject(
         model_name_or_path="m42-health/Llama3-Med42-8B",
-        knowledge_max_length=60
     )
 
+    prompt_message = "Briefly introduce kidney from a professional perspective. Focus on its shape, attribute, location in medical images and possible illnesses."
+    result_max_length = len(prompt_message.split()) + 200
+
     knowledge = model_lm_inject.generate_knowledge(
-        "Briefly introduce kidney."
+        original_prompt=prompt_message,
+        max_length=result_max_length
     )
 
     print(knowledge)
+
+    '''
+    The image is a non-contrasted computed tomography (CT) scan of the brain, showing the cerebral structures without any medical devices present. 
+    The region of interest, located centrally and in the middle of the image, exhibits an area of altered density, which is indicative of a brain hemorrhage. 
+    This area is distinct from the surrounding brain tissue, suggesting a possible hematoma or bleeding within the brain parenchyma. 
+    The location and characteristics of this abnormality may suggest a relationship with the surrounding brain tissue, potentially causing a mass effect or contributing to increased intracranial pressure.
+    '''
